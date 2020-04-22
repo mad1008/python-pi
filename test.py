2c2abae9a9f1c46a2af794aca893909c6fec72a6
mad <mouad1007>
2020-04-22 23:40:51 +0000

    Adding add function

--- test.py
+++ test.py
@@ -5,4 +5,15 @@ def dispatch(url):
 def message(msg):
 	print(msg)
 
-print(dispatch("home/user/projects/file.txt"))
+def add(*args):
+	z=0
+	for a in args:
+		try:
+			z+=a
+		except:
+			print(str(a),"is not a number")
+	return(z)
+
+
+print("Test add func",add(5,2,33,33,"t"))
+print("Test dispatch func",dispatch("home/user/projects/file.txt"))
